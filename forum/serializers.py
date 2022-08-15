1af7b5e2472bdb45602dcd4f534a94ba9d39d34f
suliman <sulimanawadstudy@gmail.com>
2022-08-16 01:13:08 +0300

    add vote_data & tags to Forum and Replies Serializers

--- forum/serializers.py
+++ forum/serializers.py
@@ -1,11 +1,29 @@
 from rest_framework import serializers
+from tag.serializers import AppliedTagSerializer
+
+from vote.models import VotedItem
 from .models import *
 
 
 class ReplySerializer(serializers.ModelSerializer):
     class Meta:
         model = Reply
-        fields = ['id', 'user_id', 'content', 'time', 'points']
+        fields = ['id', 'user_id', 'content',
+                  'time', 'points', 'vote_data', 'tags']
+
+    vote_data = serializers.SerializerMethodField()
+    tags = serializers.SerializerMethodField()
+
+    def get_vote_data(self, reply):
+        return VotedItem.objects.get_object_vote_data('reply', reply.id, self.context['user_id'])
+
+    def get_tags(self, reply):
+        return AppliedTagSerializer(AppliedTag.objects.get_object_tags(
+            'reply',
+            reply.id,
+        ),
+            many=True,
+        ).data
 
 
 class CreateReplySerializer(serializers.ModelSerializer):
@@ -23,13 +41,26 @@ class ForumSerializer(serializers.ModelSerializer):
     class Meta:
         model = Forum
         fields = ['id', 'user_id', 'title', 'content', 'is_question', 'time', 'points', 'is_closed',
-                  'closed_reply']
+                  'closed_reply', 'vote_data', 'tags']
 
-    is_closed = serializers.SerializerMethodField()
     closed_reply = ReplySerializer(read_only=True)
+    is_closed = serializers.SerializerMethodField()
+    vote_data = serializers.SerializerMethodField()
+    tags = serializers.SerializerMethodField()
 
     def get_is_closed(self, forum):
         return forum.closed_reply is not None
+
+    def get_vote_data(self, forum):
+        return VotedItem.objects.get_object_vote_data('forum', forum.id, self.context['user_id'])
+
+    def get_tags(self, forum):
+        return AppliedTagSerializer(AppliedTag.objects.get_object_tags(
+            'forum',
+            forum.id,
+        ),
+            many=True,
+        ).data
 
 
 class CreateForumSerializer(serializers.ModelSerializer):
