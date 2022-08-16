2392c94240b4bd187c5070e9130751fddfe5350c
suliman <sulimanawadstudy@gmail.com>
2022-08-16 19:47:36 +0300

    Fix a Bug

--- settings/settings/common.py
+++ settings/settings/common.py
@@ -125,8 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 
 MEDIA_URL = '/media/'
