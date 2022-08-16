c68d3724161b4760feaf0ac6f4d16657af61aa67
suliman <sulimanawadstudy@gmail.com>
2022-08-16 23:48:16 +0300

    .

--- settings/settings/common.py
+++ settings/settings/common.py
@@ -123,12 +123,29 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+STATIC_URL = '/static/'  # Didn't work
+# STATIC_URL = '/staticfiles/'  # Yes, worked
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+# MEDIA_URL = '/media/'     # Didn't work
+MEDIA_URL = '/staticfiles/media/'  # Yes, worked
+
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')     # Didn't work
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/media/')     # Yes, worked
 
 
 # Default primary key field type
