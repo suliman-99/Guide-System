fd70ab61d499f304bd5123fb4b3c7fd80f5d0885
suliman <sulimanawadstudy@gmail.com>
2022-08-17 11:53:23 +0300

    add small profile to forum and reply

--- forum/serializers.py
+++ forum/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from tag.serializers import AppliedTagSerializer
 
 from vote.models import VotedItem
+from student_profile.serializers import SmallProfileSerializer
+from student_profile.models import Profile
 from .models import *
 
 
@@ -9,16 +11,29 @@ class ReplySerializer(serializers.ModelSerializer):
     class Meta:
         model = Reply
         fields = ['id', 'user_id', 'content',
-                  'time', 'points', 'is_mine', 'vote_data']
+                  'time', 'points', 'is_mine', 'vote_data', 'profile']
 
     is_mine = serializers.SerializerMethodField()
     vote_data = serializers.SerializerMethodField()
+    profile = serializers.SerializerMethodField()
 
     def get_is_mine(self, forum):
         return bool(self.context['user_id'] and forum.user.id == self.context['user_id'])
 
     def get_vote_data(self, reply):
         return VotedItem.objects.get_object_vote_data('reply', reply.id, self.context['user_id'])
+
+    def get_profile(self, reply):
+        profiles = Profile.objects.filter(user_id=reply.user.id)
+        if profiles:
+            profile = profiles[0]
+        else:
+            profile = Profile(
+                user_id=reply.user.id,
+                gender=Profile.GENDER_MALE,
+                photo=''
+            )
+        return SmallProfileSerializer(profile).data
 
 
 class CreateReplySerializer(serializers.ModelSerializer):
@@ -36,7 +51,7 @@ class ForumSerializer(serializers.ModelSerializer):
     class Meta:
         model = Forum
         fields = ['id', 'user_id', 'title', 'content', 'is_question', 'time', 'points', 'is_closed',
-                  'closed_reply', 'is_mine', 'vote_data', 'tags', 'suggested_tags']
+                  'closed_reply', 'is_mine', 'profile', 'vote_data', 'tags', 'suggested_tags']
 
     closed_reply = ReplySerializer(read_only=True)
     is_closed = serializers.SerializerMethodField()
@@ -44,6 +59,7 @@ class ForumSerializer(serializers.ModelSerializer):
     vote_data = serializers.SerializerMethodField()
     tags = serializers.SerializerMethodField()
     suggested_tags = serializers.SerializerMethodField()
+    profile = serializers.SerializerMethodField()
 
     def get_is_closed(self, forum):
         return forum.closed_reply is not None
@@ -71,6 +87,18 @@ class ForumSerializer(serializers.ModelSerializer):
                 many=True,
             ).data
         return {}
+
+    def get_profile(self, reply):
+        profiles = Profile.objects.filter(user_id=reply.user.id)
+        if profiles:
+            profile = profiles[0]
+        else:
+            profile = Profile(
+                user_id=reply.user.id,
+                gender=Profile.GENDER_MALE,
+                photo=''
+            )
+        return SmallProfileSerializer(profile).data
 
 
 class CreateForumSerializer(serializers.ModelSerializer):
